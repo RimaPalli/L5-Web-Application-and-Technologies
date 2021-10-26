dcc860d306953fac4870eb4960c3cd0e094c2e4d
RimaPalli <53480470+RimaPalli@users.noreply.github.com>
2021-10-26 22:15:40 +0545

    Add files via upload

---
+++ Lab exercises(basic php)/watIndex.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="./style.css" rel="stylesheet" type="text/css" />
+    <title>WAT Week10</title>
+</head>
+<body>
+    <div id="pagewrap">
+        <header>
+            <h1>Web Applications and Technologies</h1>
+            <h2> Portfolio of Work </h2>
+        </header>
+        <nav>
+            <ul id="navlist">
+                <li>
+                    <a href="./basics/basics.php">Basics</a>
+                </li>
+                <li>
+                    <a href="./fundamentals/fundamentals.php">Fundamentals</a>
+                </li>
+                <li>
+                    <a href="./forms/forms.php">Forms</a>
+                </li>
+                <li>
+                    <a href="./mysql/mysql.php">MySQL</a>
+                </li>
+                <li>
+                    <a href="./crud/crud.php">CRUD</a>
+                </li>
+                <li>
+                    <a href="./session/sessions.php">Sessions</a>
+                </li>
+                <li>
+                    <a href="register.php">Register</a>
+                </li>
+                <li>
+                    <a href="sortSearch.php">Search</a>
+                </li>
+            </ul>
+        </nav>
+        <section id="content">
+            <h2>Content</h2>
+            <p>Lab Exercises </p>
+        </section>
+        <section id="sidebar">
+            <h3>A Side Panel</h3>
+        </section>
+        <footer>
+            <h4>Rima Palli C7117119</h4>
+        </footer>
+    </div>
+</body>
+</html>
